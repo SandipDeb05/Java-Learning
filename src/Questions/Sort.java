f22939d7cb84a4fb7b1205054678254d7f02a949
Sandip Deb <43350242+SandipDeb05@users.noreply.github.com>
2020-04-13 13:13:39 +0530

    Question Solutions

---
+++ src/Questions/Sort.java
@@ -0,0 +1,63 @@
+package Questions;
+
+import java.util.*;
+
+class Student implements Comparable<Student>{
+    private int id;
+    private String fname;
+    private double cgpa;
+    public Student(int id, String fname, double cgpa) {
+        super();
+        this.id = id;
+        this.fname = fname;
+        this.cgpa = cgpa;
+    }
+    public int getId() {
+        return id;
+    }
+    public String getFname() {
+        return fname;
+    }
+    public double getCgpa() {
+        return cgpa;
+    }
+
+    @Override
+    public int compareTo(Student sc) {
+        int num = Double.compare(cgpa, sc.cgpa);
+        if(num!=0) return -num;
+        num = fname.compareTo(sc.fname);
+        if(num!=0) return num;
+        num = Integer.compare(id, sc.id);
+        return num;
+    }
+}
+
+//Complete the code
+
+public class Sort
+{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+
+        List<Student> studentList = new ArrayList<Student>();
+        while(testCases>0){
+            int id = in.nextInt();
+            String fname = in.next();
+            double cgpa = in.nextDouble();
+
+            Student st = new Student(id, fname, cgpa);
+            studentList.add(st);
+
+            testCases--;
+        }
+        Collections.sort(studentList);
+        for(Student st: studentList){
+            System.out.println(st.getFname());
+        }
+    }
+}
+
+
+
