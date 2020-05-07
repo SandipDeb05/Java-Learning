f00669125a79965de89baf3d4077eb93a616f79b
Sandip Deb <43350242+SandipDeb05@users.noreply.github.com>
2020-05-07 23:44:38 +0530

    Inheritance in java

--- src/com/sandip/Main.java
+++ src/com/sandip/Main.java
@@ -2,8 +2,8 @@ package com.sandip;
 
 import java.util.Scanner;
 
-public class Main{
-    public static void main(String[] args){
+public class Main {
+    public static void main(String[] args) {
         /*Scanner sc = new Scanner(System.in);
         System.out.println("Enter your name");
         String userName = sc.nextLine();
@@ -17,9 +17,9 @@ public class Main{
         String userMsg2 = sc.nextLine();
         System.out.println("Perfect! we are great together 😁");*/
 
-/*---------------------------------------------------------------------------------------*/
-        for(int i=1; i<=10; i++){
-            for(int j=i; j>0; j--){
+        /*---------------------------------------------------------------------------------------*/
+        for (int i = 1; i <= 10; i++) {
+            for (int j = i; j > 0; j--) {
                 System.out.print(" ‍👤 ");
             }
             System.out.println();
